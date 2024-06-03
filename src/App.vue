ad0862e9a8ec7c9430c79f859630416a2cf868b0
Jessica Straub <jessica.straub@kps.com>
2024-06-03 16:12:30 +0200

    login

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <Login />
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Login from './components/log-in.vue'
-export default {
-  name: 'App',
-  components: {
-    Login,
-  },
-  data: () => ({
-    //
-  }),
-};
-</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
